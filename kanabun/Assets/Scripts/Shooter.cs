17be1d142fbd75f2b0fc75a9c365b58e221bd377
c17009Katsuki_Kinjo <c17009@std.it-college.ac.jp>
2018-07-26 12:39:15 +0900

    shooter.cs:弾が放物線を描くようにしました。 銃を2つにしました。

--- kanabun/Assets/Scripts/Shooter.cs
+++ kanabun/Assets/Scripts/Shooter.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 using Oculus.Avatar;
 using UnityEngine.UI;
 
-public class Shooter : MonoBehaviour {
+public class Shooter : MonoBehaviour
+{
     public AudioClip se;
     private AudioSource audiosource;
     public GameObject Bullet;
+    public bool Right = false;
 
     [SerializeField]
     private Image aimPointImage;
     RaycastHit hit;
 
-    void Start () {
+    void Start()
+    {
         audiosource = GetComponent<AudioSource>();
         audiosource.clip = se;
-	}
-	
-	void Update () {
+    }
+
+    void Update()
+    {
 
         Ray ray = new Ray(transform.position, transform.forward * -1);//Rayをとばす（発射座標、向き）
 
@@ -46,9 +50,16 @@ public class Shooter : MonoBehaviour {
 
         if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
         {
+            if (!Right) { return; }
             //Debug.Log("右人差し指トリガーを押した");
             //OnSound();
-            OnShot(hit.point);
+            Shoot(hit.point);
+        }
+
+        if (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger))
+        {
+            if (Right) { return; }
+            Shoot(hit.point);
         }
     }
 
@@ -59,9 +70,150 @@ public class Shooter : MonoBehaviour {
 
     private void OnShot(Vector3 aimpos)
     {
-            GameObject bullet = Instantiate(Bullet, transform.position, Quaternion.identity);
-            Rigidbody bullet_rb = bullet.GetComponent<Rigidbody>();
-            bullet_rb.rotation = transform.rotation;
-            bullet_rb.AddRelativeForce(0, 30, -500);
+        GameObject bullet = Instantiate(Bullet, transform.position, Quaternion.identity);
+        Rigidbody bullet_rb = bullet.GetComponent<Rigidbody>();
+        bullet_rb.rotation = transform.rotation;
+        bullet_rb.AddRelativeForce(0, 30, -500);
+    }
+
+    private void Shoot(Vector3 aimPos)
+    {
+        ShootFixedAngle(aimPos, 15);
+    }
+
+    private void ShootFixedAngle(Vector3 aimpos, float aimangle)
+    {
+        float speedVec = ComputeVectorFromAngle(aimpos, aimangle);
+        if (speedVec <= 0.0f)
+        {
+            Debug.Log("!!");
+            return;
+        }
+        Vector3 vec = ConvertVectorToVector3(speedVec, aimangle, aimpos);
+        InstantiateBullet(vec);
+    }
+
+    private float ComputeVectorFromAngle(Vector3 aimpos, float aimangle)
+    {
+        Vector2 startPos = new Vector2(transform.position.x, transform.position.z);
+        Vector2 targetPos = new Vector2(aimpos.x, aimpos.z);
+        float distance = Vector2.Distance(targetPos, startPos);
+
+        float x = distance;
+        float g = Physics.gravity.y;
+        float first_y = transform.position.y;
+        float y = aimpos.y;
+
+        float rad = aimangle * Mathf.Deg2Rad;//ラジアンに変換
+
+        float cos = Mathf.Cos(rad);
+        float tan = Mathf.Tan(rad);
+
+        float first_vSquare = g * x * x / (2 * cos * cos * (y - first_y - x * tan));
+        if (first_vSquare <= 0)
+        {
+            return 0;
+        }
+
+        float first_v = Mathf.Sqrt(first_vSquare);
+        return first_v;
+    }
+
+    private Vector3 ConvertVectorToVector3(float first_v, float first_angle, Vector3 aimpos)
+    {
+        Vector3 startPos = transform.position;
+        Vector3 targetPos = aimpos;
+        startPos.y = 0.0f;
+        targetPos.y = 0.0f;
+
+        Vector3 dir = (targetPos - startPos).normalized;
+        Quaternion yawRot = Quaternion.FromToRotation(Vector3.right, dir);
+        Vector3 vec = first_v * Vector3.right;
+
+        vec = yawRot * Quaternion.AngleAxis(first_angle, Vector3.forward) * vec;
+
+        return vec;
+    }
+
+    private void InstantiateBullet(Vector3 aimVector)
+    {
+        if (Bullet == null)
+        {
+            throw new System.NullReferenceException("Bullet is null");
+        }
+
+        var obj = Instantiate<GameObject>(Bullet, transform.position,
+            Quaternion.identity);
+        var rigidbody = obj.GetComponent<Rigidbody>();
+
+        Vector3 force = aimVector * rigidbody.mass;
+        rigidbody.AddForce(force, ForceMode.Impulse);
+    }
+
+    private void ShootFixedTime(Vector3 aimPos,float airTime)
+    {
+        float speedVec = ComputeVectorFormTime(aimPos, airTime);
+        float angle = ComputeVectorFromAngle(aimPos, airTime);
+
+        if(speedVec <= 0.0f)
+        {
+            Debug.Log("!!");
+            return;
+        }
+
+        Vector3 vec = ConvertVectorToVector3(speedVec, angle, aimPos);
+        InstantiateBullet(vec);
+    }
+
+    private float ComputeVectorFormTime(Vector3 aimPos,float airTime)
+    {
+        Vector2 vec = ComputeVectorXYFromTime(aimPos, airTime);
+
+        float v_x = vec.x;
+        float v_y = vec.y;
+
+        float first_vSquare = v_x * v_x + v_y * v_y;
+        if(first_vSquare <= 0.0f)
+        {
+            return 0.0f;
+        }
+
+        float first_v = Mathf.Sqrt(first_vSquare);
+        return first_v;
+    }
+
+    private float ComputeAngleFromTime(Vector3 aimPos,float airTime)
+    {
+        Vector2 vec = ComputeVectorXYFromTime(aimPos, airTime);
+        float v_x = vec.x;
+        float v_y = vec.y;
+
+        float rad = Mathf.Atan2(v_y, v_x);
+        float angle = rad * Mathf.Rad2Deg;
+
+        return angle;
+    }
+
+    private Vector2 ComputeVectorXYFromTime(Vector3 aimPos,float airTime)
+    {
+        if(airTime <= 0.0f)
+        {
+            return Vector2.zero;
+        }
+
+        Vector2 startPos = new Vector2(transform.position.x, transform.position.z);
+        Vector2 targetPos = new Vector2(aimPos.x, aimPos.z);
+        float distance = Vector2.Distance(targetPos, startPos);
+
+        float x = distance;
+        float g = -Physics.gravity.y;
+        float first_y = transform.position.y;
+        float y = aimPos.y;
+        float t = airTime;
+
+        float v_x = x / t;
+        float v_y = (y - first_y) / t + (g * t) / 2;
+
+        return new Vector2(v_x, v_y);
     }
 }
