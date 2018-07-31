7ef2e40491fa280ded46c2c5ac8d7f4ddb7cab70
MYCOMPUTER\c17014 <c17014@std.it-college.ac.jp>
2018-07-31 12:40:26 +0900

    Bossのスクリプトを書いてます(50%)

--- kanabun/Assets/terukina/Script/Boss.cs
+++ kanabun/Assets/terukina/Script/Boss.cs
@@ -3,24 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Boss : MonoBehaviour {
-    public int BossLife = 60;
+    public int BossLife = 90;
     public Transform Player;
-    private float Bossspeed = 0.01f;
+    public Transform TargetPoint;
+    private float Bossspeed = 0.1f;
+    bool Change = true;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.LookAt(Player);
-        transform.position += transform.forward * Bossspeed;
-		if(BossLife == 0)
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BossLife <= 60 && BossLife >=50)
         {
-            Destroy(this.gameObject);
+            Change = false;
+            Target();
         }
-	}
+        else if (BossLife<= 0)
+        {
+             Destroy(this.gameObject);
+        }
+        if(Change == true)
+        {
+            Move();
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Bullet")
@@ -31,7 +41,21 @@ public class Boss : MonoBehaviour {
         if(other.gameObject.tag == "MainCamera")
         {
             Destroy(other.gameObject);
-
+        }
+    }
+    void Move()
+    {
+        transform.LookAt(Player);
+        transform.position += transform.forward * Bossspeed;
+    }
+    void Target()
+    {
+        Bossspeed = 0.5f;
+        transform.LookAt(TargetPoint);
+        transform.position += transform.forward * Bossspeed;
+        if(this.transform.position == TargetPoint.position)
+        {
+            Change = true;
         }
     }
 
--- kanabun/Assets/terukina/Script/Pshot.cs
+++ kanabun/Assets/terukina/Script/Pshot.cs
@@ -14,15 +14,24 @@ public class Pshot : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetKey(KeyCode.Z) || Input.GetButton("Fire1"))
         {
             Shot();
         }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -1, 0);
+        }
+
 	}
 
     void Shot()
     {
-        var BulletIns = GameObject.Instantiate(bullet,transform.position, Quaternion.identity) as GameObject;
+        var BulletIns = GameObject.Instantiate(bullet,this.transform.position, transform.rotation) as GameObject;
         BulletIns.GetComponent<Rigidbody>().AddForce(BulletIns.transform.forward * BulletPower);
         Destroy(BulletIns, 5f);
     }
