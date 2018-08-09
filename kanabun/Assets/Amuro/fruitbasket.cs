a49bf3894cfe738436212ae0628de325531e2c44
amuroo <c17001@std.it-college.ac.jp>
2018-08-09 16:16:18 +0900

    フルーツの取られていく挙動の追加

--- kanabun/Assets/Amuro/fruitbasket.cs
+++ kanabun/Assets/Amuro/fruitbasket.cs
@@ -12,11 +12,18 @@ public class fruitbasket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        print(fruit.Count);
 	}
 
     private void OnTriggerEnter(Collider other)
     {
-    
+            if (other.gameObject.tag == "Enemy" && 0 < fruit.Count )
+            {                
+                //GameObject Enemy = GameObject.FindWithTag("Enemy");
+                fruit[0].transform.parent = other.transform;
+                fruit[0].transform.position = other.transform.position;
+                fruit[0].transform.position += new Vector3(0, 1, 0);
+                fruit.RemoveAt(0);
+            }
     }
 }
--- kanabun/Assets/terukina/Script/EnemyTest_pro.cs
+++ kanabun/Assets/terukina/Script/EnemyTest_pro.cs
@@ -6,15 +6,12 @@ public class EnemyTest_pro : MonoBehaviour
 {
     public Transform Target;
     public float EnemySpeed;
-    public static int EnemyDcount;
-
-    public fruitbasket fb;
-    
+  
 
     // Use this for initialization
     void Start()
     {
-       //List<GameObject> fruit = fb.fruit;
+     
     }
 
     // Update is called once per frame
@@ -22,31 +19,18 @@ public class EnemyTest_pro : MonoBehaviour
     {
         /*transform.LookAt(Target);
         transform.position += transform.forward * EnemySpeed;*/
+        
     }
 
 
-    private void OnTriggerEnter(Collider other)
+    /*private void OnTriggerEnter(Collider other)
     {
-        //切り株に触れたら、フルーツの親になる
-        //for (int i = fb.fruit.Count; 0 < i; i--)
-        //{
-
-            if (other.gameObject.tag == "Stump")
-            {
-                GameObject Fruit = GameObject.FindWithTag("Fruit");
-                Fruit.transform.parent = transform;
-                Fruit.transform.position = transform.position;
-                Fruit.transform.position += new Vector3(0, 1, 0);
-                fb.fruit.RemoveAt(-1);
-                print("OK");
-            }
-       // }
-
+       
         if (other.gameObject.tag == "Bullet")
         {
             Destroy(this.gameObject);
             Destroy(other.gameObject);
             EnemyDcount += 1;
         }
-    }
+    }*/
 }
